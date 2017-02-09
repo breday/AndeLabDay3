142d0e9be8c082f292ee2c9f86de89597f3dfebe
breday <brndnyokabi@gmail.com>
2017-02-09 22:39:20 +0300

    find_missing.py

---
+++ find_missing.py
@@ -0,0 +1,22 @@
+#define a function that takes two arguments
+def find_missing(A,B):
+	#convert list A,B into set
+	if isinstance (A,list):
+		x=set(A)
+		if isinstance(B,list):
+			y=set(B)
+
+#perform set operation
+			for i in x:
+				if x and y == ():#check if set is empty
+					return 0
+				elif x  == y:#check if set x and y have the same value
+					return 0
+				else:#gets the asymentrical difference and prints it
+					x ^ y
+					return i()
+
+					
+		
+
+
